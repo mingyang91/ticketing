21602908114801008ea2e90aeae8e400384e59a9
My <my@famer.me>
2021-06-11 22:21:15 +0800

    [benchmark] 1500 TPS.

--- src/benchmark/benchmark.controller.ts
+++ src/benchmark/benchmark.controller.ts
@@ -1,7 +1,8 @@
 import { OrderState } from '.prisma/client';
 import { Controller, Get } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
-import { range } from 'ramda';
+import { flatten, range } from 'ramda';
+import { Flight } from '@prisma/client';
 
 @Controller()
 class BenchmarkController {
@@ -13,21 +14,24 @@ class BenchmarkController {
 
   static readonly ROUTES = 50;
   static readonly TRAVELERS = 10000;
-  static readonly FLIGHTS = 200;
+  static readonly FLIGHTS = 2000;
   static readonly AIRPORTS = 1000;
   static readonly TICKETS = 8000;
 
-  @Get('/private/api/benchmark/generate')
+  @Get('/private/api/benchmark/reset')
+  async reset(): Promise<number> {
+    return await this.clearAll() + await this.generateAll();
+  }
+
   async generateAll(): Promise<number> {
     const routes = await this.generateRoutes();
     const travelers = await this.generateTravelers();
     const airports = await this.generateAirports();
     const flights = await this.generateFlight();
-    const tickets = await this.generateTickets();
-    return routes + travelers + airports + flights + tickets;
+    const tickets = await this.generateTickets(flights);
+    return routes + travelers + airports + flights.length + tickets;
   }
 
-  @Get('/private/api/benchmark/clear')
   async clearAll(): Promise<number> {
     const tickets = await this.prisma.ticket.deleteMany();
     const flights = await this.prisma.flight.deleteMany();
@@ -93,29 +97,24 @@ class BenchmarkController {
     return res.count;
   }
 
-  async generateTickets(): Promise<number> {
-    const tickets = BenchmarkController.serial(
-      BenchmarkController.TICKETS,
-      5,
-    ).map((sn, index) => {
-      return {
-        id: 'ticket#' + sn,
-        flightId:
-          'flight#' +
-          ((index % BenchmarkController.FLIGHTS) + 1)
-            .toString()
-            .padStart(3, '0'),
-        price: (300 + Math.random() * 1000) | 0,
-        orderState: OrderState.FREE,
-      };
+  async generateTickets(flights: Flight[]): Promise<number> {
+    const tickets = flights.map((flight) => {
+      return BenchmarkController.serial(flight.capacity, 3).map((sn) => {
+        return {
+          id: 'ticket#' + flight.id + sn,
+          flightId: flight.id,
+          price: flight.basePrice + Math.random() * 1000 | 0,
+          orderState: OrderState.FREE
+        }
+      })
     });
 
-    const res = await this.prisma.ticket.createMany({ data: tickets });
+    const res = await this.prisma.ticket.createMany({ data: flatten(tickets) });
 
     return res.count;
   }
 
-  async generateFlight(): Promise<number> {
+  async generateFlight(): Promise<Flight[]> {
     const flights = BenchmarkController.serial(
       BenchmarkController.FLIGHTS,
       3,
@@ -134,7 +133,7 @@ class BenchmarkController {
 
     const res = await this.prisma.flight.createMany({ data: flights });
 
-    return res.count;
+    return flights;
   }
 }
 
